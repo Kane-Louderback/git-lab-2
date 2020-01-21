a9593c96d0959e32f01719da9638a58dc7b20234
Kane-Louderback <kl505918@ohio.edu>
2020-01-21 11:08:42 -0500

    Committed git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kane Louderback
+ *        Date: 1/21/2020
+ * Description: Git Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
